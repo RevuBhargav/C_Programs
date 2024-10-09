ee18bf48f24de1bc1d2a4f163374cfd8769428cc
RevuBhargav <bhargavrevu58@gmail.com>
2024-10-09 13:26:14 +0530

    09/10/2024(sum while loop)

---
+++ .dist/Sum of Natural Numbers Using while Loop.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,i, sum =0;
+    printf("Enter a natural num : ");
+    scanf("%d",&n);
+    i = 1;
+    while(i<=n){
+        sum += i:
+        i++;
+    }
+    printf("Sum = %d\n",sum);
+}
