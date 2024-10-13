e172bd95c3cbcbf275a425abd56b3f726c3fce25
RevuBhargav <bhargavrevu58@gmail.com>
2024-10-13 20:51:07 +0530

    Maximum value of three integers

---
+++ .dist/Maximum value of three integers.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int num1,num2,num3,max;
+	printf("Input the first integer :");
+	scanf("%d",&num1);
+	printf("Input the second integer: ");
+	scanf("%d",&num2);
+	printf("Input the third integer: ");
+	scanf("%d",&num3);
+	
+	max = num1;
+	
+	if(num2>max){
+		max = num2;
+	}
+	if(num3 > max){
+		max = num3;
+	}
+	printf("Maximum value of three integers: %d\n",max);
+       
+}
