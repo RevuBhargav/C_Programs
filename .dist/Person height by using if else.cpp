f9e32916f0e78023f1c38cf8a96963964937ab5f
RevuBhargav <bhargavrevu58@gmail.com>
2024-10-13 20:59:01 +0530

    persin height if else

---
+++ .dist/Person height by using if else.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int height;
+	printf("enter the person height :");
+	scanf("%d",&height);
+	if(height<150){
+		printf("The person is Dwarf.");
+		
+	}else
+	printf("the person is height");
+	
+}
