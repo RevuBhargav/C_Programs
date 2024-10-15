9721abd91f65529e12fb0ac10e3b8a822c87f0cb
RevuBhargav <bhargavrevu58@gmail.com>
2024-10-15 13:13:36 +0530

    switch casses

---
+++ .dist/weekly Switch casses.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+   int n;
+   printf("enter a value : ");
+   scanf("%d",&n);
+    switch(n){
+        case 1 : 
+        printf("sunday");
+        break;
+        case 2 :
+        printf("monday");
+        break;
+        case 3 :
+        printf("thuesday");
+        break;
+        case 4 :
+        printf("wed");
+        break;
+        case 5 :
+        printf("thu");
+        break;
+        case 6 :
+        printf("fri");
+        break;
+        case 7 :
+        printf("sat");
+        break;
+        
+    }
+}
