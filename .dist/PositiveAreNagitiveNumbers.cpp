6482b117d88943aec7052631017eceb610a7bf43
RevuBhargav <bhargavrevu58@gmail.com>
2024-10-15 12:38:34 +0530

    positive and nagitive num

---
+++ .dist/PositiveAreNagitiveNumbers.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter a value : ");
+    scanf("%d",&a);
+    if(a>=0){
+        printf("%d is positive numbers",a);
+    }else{
+      printf("%d is nagitive number",a);
+    }
+}
